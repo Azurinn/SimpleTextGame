0699471cf2acf16233a50a9ccd46fcd86db7f407
Azurinn <98115260+Azurinn@users.noreply.github.com>
2024-04-27 22:03:15 +0200

    Try, catch, throw
    Exercises with Try, catch, throw

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -23,15 +23,23 @@ function getCurrentResult() {
   }
 }
 
-
-const enteredValue = prompt('Maximum life for you and the monster.','100');
-let chosenMaxLife = parseInt(enteredValue);
-
 let battleLog = [];
+function getMaxLifeValues(){
+  const enteredValue = prompt('Maximum life for you and the monster.','100');
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw {message : 'Invalid user input, not a number!'};
+  }
+  return parsedValue;
+}
+let chosenMaxLife = getMaxLifeValues();
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error){
+  console.log(error);
   chosenMaxLife = 100;
-  alert('You entered the wrong value, the default value was set! ');
+  alert('You entered something wrong, default value of 100 was used.');
 }
 
 let currentMonsterHealth = chosenMaxLife;
